49cab9f62fd349d3bb3650b6ded57f7617ae4de7
Ronin <46785440+byronin@users.noreply.github.com>
2024-02-03 14:15:21 -0600

    Update LoRaSender_Receiver.ino
    tests

--- Software/LoRaSender_Receiver/LoRaSender_Receiver.ino
+++ Software/LoRaSender_Receiver/LoRaSender_Receiver.ino
@@ -42,3 +42,51 @@ void loop() {
   }
 
 }
+import paho.mqtt.client as mqtt
+import serial
+import time
+
+# MQTT Settings
+broker_address = "broker.hivemq.com"
+port = 1883
+topic = "raspi/robot"
+
+# Serial port settings (Adjust these settings according to your system)
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+ser.flush()
+
+# Function to be called when a message is received from MQTT
+def on_message(client, userdata, message):
+    msg = str(message.payload.decode("utf-8"))
+    print(f"Message received from MQTT: {msg}")
+    ser.write(msg.encode('utf-8'))  # Write to serial port
+
+# Read data from serial and publish to MQTT
+def read_from_serial_and_publish(client):
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8').rstrip()
+        print(f"Data received from serial: {line}")
+        client.publish(topic, line)
+
+# Set up MQTT client
+client = mqtt.Client("Raspi_Robot_Client")
+client.on_message = on_message
+
+# Connect to the broker
+client.connect(broker_address, port=port)
+client.subscribe(topic)
+
+# Start listening for messages asynchronously
+client.loop_start()
+
+try:
+    while True:
+        read_from_serial_and_publish(client)
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Program terminated")
+
+# Cleanup
+client.loop_stop()
+client.disconnect()
+ser.close()
